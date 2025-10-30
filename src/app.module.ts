1ced1a293a5627de7f6502e3c217bf6df522fb9e
annaivv <annashalya@gmail.com>
2025-10-30 20:25:31 +0100

    feature: user adding logic & dto added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModule } from './company/company.module';
+import { UserModule } from './user/user.module';
 import ormconfig from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), CompanyModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), CompanyModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
