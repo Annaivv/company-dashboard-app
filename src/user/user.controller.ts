4df5e7e91d989b9cf5c575fd387812ae463b8e8f
annaivv <annashalya@gmail.com>
2025-11-05 14:48:39 +0100

    feature: validation for user creation added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
 import { CreateUserDto } from '@/user/dto/createUser.dto';
 import { UserService } from '@/user/user.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post()
   @ApiOperation({ summary: 'Create a user' })
   @ApiResponse({ status: 201, description: 'Returns a new user' })
